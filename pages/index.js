12e9084eb796d484e5c6e62463e49b7aa7a5062c
JoaoPedroADC <134790598+JoaoPedroADC@users.noreply.github.com>
2026-07-18 04:14:33 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>João Pedro, corra atrás dos seus sonhos. Se fizer isso vai conquistar o mundo.</h1>
+    return <h1>João Pedro, corra atrás dos seus sonhos. Se fizer isso, vai conquistar o mundo e isso vai ser incrível.</h1>
 }
 
 export default Home;
